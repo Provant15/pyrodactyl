6a3f0d70e63a02ba8896da6c144413ec3435c8b3
Provant15 <me@provant.dev>
2026-03-13 17:50:41 +1000

    feat(admin): expose admin_permissions in AccountTransformer for frontend

--- app/Transformers/Api/Client/AccountTransformer.php
+++ app/Transformers/Api/Client/AccountTransformer.php
@@ -16,6 +16,10 @@ class AccountTransformer extends BaseClientTransformer
 
     /**
      * Return basic information about the currently logged-in user.
+     *
+     * The `admin_permissions` field contains the union of all permission strings
+     * granted to the user via their assigned roles. For non-admin users this is
+     * an empty array.
      */
     public function transform(User $model): array
     {
@@ -27,6 +31,7 @@ class AccountTransformer extends BaseClientTransformer
             'first_name' => $model->name_first,
             'last_name' => $model->name_last,
             'language' => $model->language,
+            'admin_permissions' => $model->adminPermissions(),
         ];
     }
 }
